b154f41ae77e331dd6b50e008ebff42e01e12860
arvindkalra <kalarvind97@gmail.com>
2017-10-12 14:25:59 +0530

    .

--- server.js
+++ server.js
@@ -3,11 +3,17 @@
  */
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const port = process.env.PORT;
+const port = 2000 || process.env.PORT;
 
-app.get('/what', function (req, res) {
-    var obj = {id : req.query.name};
+app.post('/rohan', function (req, res) {
+    var name = req.body.name;
+    var newname = name + "HIII..";
+    var obj = {id : newname};
+
     res.send(obj);
 });
 
